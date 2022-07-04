377eeb97134fc5505c09c2eac6772420211f4021
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 02:22:01 -0300

    fix: formula calculando valor de circuferencia de uma area

--- 01_fundamentos/tipos/variaveis.py
+++ 01_fundamentos/tipos/variaveis.py
@@ -9,10 +9,14 @@ b = 4
 texto = 'Sua idade é ... '
 idade = 44
 
-raio = float(input('Informe o raio da circuferencia: '))
+PI = 3.14 # NAO EXISTE CONSTANTE EM PYTHON :: POR CONVERSAO REPRESENTAMOS UMA COM TUDO MAIUSCULO.
 
-print(raio)
-print(type(raio))
+raio = float(input('Informe o raio da circuferencia: '))
+area = PI * raio * raio
+
+# print(raio)
+# print(type(raio))
+print(area)
 
 # print(3 * 'bom dia...') # multiplicando valores de variaveis
 # print(4 * texto) # repetindo valores de variaveis com operacao matematica
