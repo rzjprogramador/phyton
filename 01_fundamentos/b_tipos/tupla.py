8f5887e390401653edaa84f844559e53fc70da62
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 11:28:37 -0300

    OK

--- 01_fundamentos/b_tipos/tupla.py
+++ 01_fundamentos/b_tipos/tupla.py
@@ -11,8 +11,9 @@ print('reinaldo' in nomes)
 # vai retornar False porque o item na tupla comeca com letra Maiuscula é case sensitive o operador in tem que passar exatamente a pesquisa
 
 # ----------------------------------------------------------------------------
-# -----------------------/*  ACESSOS  */-----------------------
+# -----------------------/*  ACESSOS PESQUISA DENTRO DE LISTAS E TUPLAS */-----------------------
 # conceito posicoes dentro dos colchetes: 
+# A direita passamos <INICIO> :  a esquerda passamos <FIM> da pesquisa
 
 # Posicao a esquerda antes da atribuicao pega a posicao passada
 # "NADA PASSADO" A DIREITA APOS OS 2PONTOS PEGA O ULTIMO
@@ -33,6 +34,12 @@ print(f'Do SEGUNDO ao PENULTIMO item da tupla nomes são: {nomes[1:-1]}')
 
 # pegando do SEGUNDO AO ULTIMO item da tupla
 print(f'Do SEGUNDO ao ULTIMO item da tupla nomes são: {nomes[1:]}') 
+
+# pegando todos apartir do 2º item
+print(f'Pegando todos apartir do 2º item {nomes[2:]}')
+
+# PASSANDO NADA A ESQUERDA INICIO :: pegando todos apartir do 2º item
+print(f'PASSANDO NADA A ESQUERDA INICIO ::  {nomes[:2]}')
 
 # pegando o ultimo item da tupla
 print(f'O ultimo item da tupla nomes é: {nomes[-1]}') 
