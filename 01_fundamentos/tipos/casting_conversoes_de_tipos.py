c4b6c11d727621b6e68bc59825f3045760943565
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 01:56:16 -0300

    shebang python

---
+++ 01_fundamentos/tipos/casting_conversoes_de_tipos.py
@@ -0,0 +1,8 @@
+
+# type(<target>)  :: type() mostra qual a classe do tipo alvo passado.
+# str(<targetAlvo>)  :: str() converte para string o alvo passado
+# int(<targetAlvo>)  :: str() converte para numero inteiro o alvo passado
+
+onze = int('11')
+print(onze)
+print(type(onze))
--- 01_fundamentos/tipos/variaveis.py
+++ 01_fundamentos/tipos/variaveis.py
@@ -1,13 +1,20 @@
 
-a = 3
-b = 4
-
 comentarioEmVariavel = """ 
   atribui um comentario em variavel de valor string
 """
 
+a = 3
+b = 4
+
+texto = 'Sua idade é ... '
+idade = 44
 
 
 
-print(a + b)
+print(texto + str(idade)) # modo certo concatenar variaeis diferentes é fazendo casting convertendo o tipo
+
+# print(texto + idade) # vai gerar erro: TypeError: can only concatenate str (not "int") to str :: porque ele nao concatena variaveis de tipos diferentes ex: string e numero
+
+# print(a + b)
+
 # print(comentarioEmVariavel)
