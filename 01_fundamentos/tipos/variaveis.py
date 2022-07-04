efc626f41f11f7e9c2dd20a1258ad180cad585fc
rzjprogramador <rzjprogramador@gmail.com>
2022-07-04 02:03:42 -0300

    fix: multiplicando valores de variaveis

--- 01_fundamentos/tipos/variaveis.py
+++ 01_fundamentos/tipos/variaveis.py
@@ -9,9 +9,13 @@ b = 4
 texto = 'Sua idade é ... '
 idade = 44
 
+raio = input('Informe o raio da circuferencia: ')
 
+# print(3 * 'bom dia...') # multiplicando valores de variaveis
+# print(4 * texto) # repetindo valores de variaveis com operacao matematica
 
-print(texto + str(idade)) # modo certo concatenar variaeis diferentes é fazendo casting convertendo o tipo
+# print(f'{texto} {idade}') # melhor forma usar FString : para fazer interpolacao mostrar variaveis na string
+# print(texto + str(idade)) # concatenar variaeis diferentes é fazendo casting convertendo o tipo
 
 # print(texto + idade) # vai gerar erro: TypeError: can only concatenate str (not "int") to str :: porque ele nao concatena variaveis de tipos diferentes ex: string e numero
 
