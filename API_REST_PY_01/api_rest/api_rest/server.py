b63494d4f77fc8cd6cb202bf0f993af4fa6d2d06
rzjprogramador <rzjprogramador@gmail.com>
2022-07-06 11:44:11 -0300

    fix: rotas rest com consultas vias parametros

--- API_REST_PY_01/api_rest/api_rest/server.py
+++ API_REST_PY_01/api_rest/api_rest/server.py
@@ -8,3 +8,22 @@ async def root():
   return {
     'mensagem': 'Hello Fast Api >> 1 ---'
   }
+
+@app.get('/postar/{nome}/{sobrenome}/{idade}')
+async def postar(nome, sobrenome, idade):
+  return {
+    'nome': nome,
+    'sobrenome': sobrenome,
+    'idade': idade
+}
+
+@app.get('/saudacao/{mensagem}')
+async def saudacao(mensagem: str):
+  texto = f'Ola {mensagem}'
+  return { 'saudacao': texto}
+
+@app.get('/quadrado/{numero}')
+async def quadrado(numero: int):
+  result = numero * numero
+  texto = f'O Quadrado de {numero} é {result}'
+  return {'resultado': texto}
