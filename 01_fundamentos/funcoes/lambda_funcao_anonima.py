0c0bf9ef358c369ecbf4b5ce232c26e952f2fdf3
rzjprogramador <rzjprogramador@gmail.com>
2022-07-09 23:23:48 -0300

    fix: loop map

---
+++ 01_fundamentos/funcoes/lambda_funcao_anonima.py
@@ -0,0 +1,22 @@
+
+# exemplo
+
+lista2 = [2, 3, 4]
+
+potencializados = map(lambda x: x * x, lista2)
+print(list(potencializados))
+
+
+
+"""
+LAMBDA - FUNCAO ANONIMA
+
+sintaxe_ lambda <recebe>: <devolve recebido>, <alvo>
+
+conceito: recebe um parametro, devolve este parametro operado modificado, no 2º paramtero a fonte de dados alvo
+
+narracao: lambda "A CADA VOLTA" "VOU RECEBER ESTE PARAM" : "E VOU VOU DEVOLVER ESTE PARAM DESTA FORMA OPERADO "
+
+# obs foi usado o map() porque no for nao da pra fazer operacoes com o iterador
+
+"""
--- 01_fundamentos/refactor/loops_python_moderno.py
+++ 01_fundamentos/refactor/loops_python_moderno.py
@@ -8,7 +8,7 @@ lista = [1, 2, 3, 4, 5]
 novaLista = filter(lambda x: x > 2, lista) 
 print(list(novaLista))
 
-# LEGADO FILTRAGEM 
+# LEGADO FILTRAGEM COM FOR 
 # for i in lista:
 #   if i > 2:
 #     print(i)
