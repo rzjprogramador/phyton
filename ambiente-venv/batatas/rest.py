34e96efa937f2e3c19e71702500ce76baa73716b
rzjprogramador <rzjprogramador@gmail.com>
2022-07-18 10:55:57 -0300

    fix: fastapi com basemodel pydantic

--- ambiente-venv/batatas/rest.py
+++ ambiente-venv/batatas/rest.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from exemplo_01 import Exemplo01, InserirExemploNaTabela
 
 app = FastAPI()
 
 @app.get('/home')
 def home():
   return { 'message': 'Alo Home'}
+
+@app.post('/criar-exemplo')
+def criarExemplo(request: Exemplo01):
+  resposta = InserirExemploNaTabela(request)
+  return resposta
