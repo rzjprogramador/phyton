828beaa01e7af9a3aef8f7b44b040b5129bf7a34
rzjprogramador <rzjprogramador@gmail.com>
2022-07-18 12:18:16 -0300

    ok

--- ambiente-venv/batatas/rest.py
+++ ambiente-venv/batatas/rest.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from exemplo_classes.classe_exemplo_02 import Exemplo03
-# from exemplo_01 import Exemplo01, InserirExemploNaTabela
+from exemplo_02.exemplo_01 import Exemplo01, InserirExemploNaTabela
 
 app = FastAPI()
 
@@ -9,8 +9,8 @@ def home():
   return { 'message': 'Alo Home'}
 
 @app.post('/criar-exemplo')
-def criarExemplo(request: Exemplo03):
+def criarExemplo(request: Exemplo01):
   print(request)
-  return request
-  # resposta = InserirExemploNaTabela(request)
-  # return resposta
+  # return request
+  resposta = InserirExemploNaTabela(request)
+  return resposta
